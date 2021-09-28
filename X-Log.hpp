89857dc5aaa7866f3981e8f0fbc3dc9c1e44d3a9
Joe <71909018+Xenovia02@users.noreply.github.com>
2021-09-28 18:52:52 +0000

    updated X-Log.hpp macros

--- X-Log.hpp
+++ X-Log.hpp
@@ -9,21 +9,21 @@
 xlog::Logger main_lgr = xlog::Logger(std::string("main"), std::cout.rdbuf());
 #endif
 
-#ifndef X_LOG_NO_AUTO_LOG_MACRO
-#define X_LOG_NO_AUTO_LOG_MACRO
-#define LOG(msg, lvl) logger.log({lvl, #msg});
-#endif
-
 #ifndef X_LOG_NO_AUTO_INFO
 #define X_LOG_NO_AUTO_INFO
-#define __INFO__ xlog::FormatInfo { __FILE__, __LINE__ }
+#define __INFO__  __FILE__, __LINE__ 
+#endif
+
+#ifndef X_LOG_NO_AUTO_LOG_MACRO
+#define X_LOG_NO_AUTO_LOG_MACRO
+#define LOG(msg, lvl, ...) logger.log(#msg, #lvl, { __INFO__, { #__VA_ARGS__ } });
 #endif
 
 #ifdef X_LOG_AUTO_THROW_LOG
 #ifndef CX_LOG_THROW_MSG
 #define CX_LOG_THROW_MSG "Error thrown"
 #endif
-#define throw xlog::Logger::log_all({ CX_LOG_THROW_MSG, xlog::ERROR }, __INFO__); throw 
+#define throw xlog::Logger::log_all( CX_LOG_THROW_MSG, 100, { __INFO__ }); throw up
 #endif
 
 #endif
