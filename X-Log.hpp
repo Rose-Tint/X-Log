73ecdb62fb2c450e0d6f9ca431d2e77d9802318b
Joe <71909018+Xenovia02@users.noreply.github.com>
2021-10-06 13:42:08 +0000

    renamed __INFO__ macro to RCD_INFO

--- X-Log.hpp
+++ X-Log.hpp
@@ -21,19 +21,19 @@ xlog::Logger main_lgr = xlog::Logger(std::string("main"), std::cout.rdbuf());
 
 #ifndef X_LOG_NO_AUTO_INFO
 #define X_LOG_NO_AUTO_INFO
-#define __INFO__  __FILE__, __LINE__ 
+#define RCD_INFO __FILE__ , __LINE__ 
 #endif
 
 #ifndef X_LOG_NO_AUTO_LOG_MACRO
 #define X_LOG_NO_AUTO_LOG_MACRO
-#define LOG(msg, lvl, ...) logger.log(#msg, #lvl, { __INFO__, { #__VA_ARGS__ } });
+#define LOG(msg, lvl, ...) logger.log(#msg, #lvl, { RCD_INFO, { #__VA_ARGS__ } });
 #endif
 
 #ifdef X_LOG_AUTO_THROW_LOG
 #ifndef CX_LOG_THROW_MSG
 #define CX_LOG_THROW_MSG "Error thrown"
 #endif
-#define throw xlog::Logger::log_all( CX_LOG_THROW_MSG, 100, { __INFO__ }); throw up
+#define throw xlog::Logger::log_all( CX_LOG_THROW_MSG, 100, { RCD_INFO }); throw up
 #endif
 
 #endif
