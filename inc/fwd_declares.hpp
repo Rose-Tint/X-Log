ceec2f81c49d8faf96d4aebd805079e057f708b0
Joe <71909018+Xenovia02@users.noreply.github.com>
2021-10-07 16:20:56 +0000

    added a constructor for c++20 only

--- inc/fwd_declares.hpp
+++ inc/fwd_declares.hpp
@@ -24,64 +24,58 @@
 
 // ensure that filesystem xor experimental filesystem is included
 #ifndef INC_STD_FS_EXP
-#ifdef __cpp_lib_filesystem
-#define INC_STD_FS_EXP 0
-#elif defined( __cpp_lib_experimental_filesystem )
-#define INC_STD_FS_EXP 1
-#elif !defined( __has_include )
-#define INC_STD_FS_EXP 1
-#elif __has_include(<filesystem>)
-#ifdef _MSC_VER
-#if __has_include(<yvals_core.h>)
-#include <yvals_core.h>
-#if defined(_HAS_CXX17) && _HAS_CXX17
-#define INC_STD_FS_EXP 0
-#endif
-#endif
-#ifndef INC_STD_FS_EXP
-#define INC_STD_FS_EXP 1
-#endif
-#else
-#define INC_STD_FS_EXP 0
-#endif
-#elif __has_include( <experimental/filesystem> )
-#define INC_STD_FS_EXP 1
-#else
-#error Could not find system header "<filesystem>" or "<experimental/filesystem>"
-#endif
-#if INC_STD_FS_EXP
-#include <experimental/filesystem>
-namespace xlog { namespace fs = std::experimental::filesystem; }
-#undef INC_STD_FS_EXP
-#else
-#include <filesystem>
-namespace xlog { namespace fs = std::filesystem; }
-#undef INC_STD_FS_EXP
-#endif
+#  ifdef __cpp_lib_filesystem
+#    define INC_STD_FS_EXP 0
+#  elif defined( __cpp_lib_experimental_filesystem )
+#    define INC_STD_FS_EXP 1
+#  elif !defined( __has_include )
+#    define INC_STD_FS_EXP 1
+#  elif __has_include(<filesystem>)
+#    ifdef _MSC_VER
+#      if __has_include(<yvals_core.h>)
+#        include <yvals_core.h>
+#        if defined(_HAS_CXX17) && _HAS_CXX17
+#          define INC_STD_FS_EXP 0
+#        endif
+#      endif
+#      ifndef INC_STD_FS_EXP
+#        define INC_STD_FS_EXP 1
+#      endif
+#    else
+#      define INC_STD_FS_EXP 0
+#    endif
+#  elif __has_include( <experimental/filesystem> )
+#    define INC_STD_FS_EXP 1
+#  else
+#    error Could not find system header "<filesystem>" or "<experimental/filesystem>"
+#  endif
+#  if INC_STD_FS_EXP
+#    include <experimental/filesystem>
+     namespace xlog { namespace fs = std::experimental::filesystem; }
+#  else
+#    include <filesystem>
+     namespace xlog { namespace fs = std::filesystem; }
+#  endif
 #endif
 
 
 #if __cplusplus > 201703L
-
-#ifndef LIKELY
-#define LIKELY [[likely]]
-#endif
-#ifndef UNLIKELY
-#define UNLIKELY [[unlikely]]
-#endif
-
+#  ifndef LIKELY
+#    define LIKELY [[likely]]
+#  endif
+#  ifndef UNLIKELY
+#    define UNLIKELY [[unlikely]]
+#  endif
 #else
-
-#ifndef LIKELY
-#define LIKELY
-#endif
-#ifndef UNLIKELY
-#define UNLIKELY
-#endif
-#ifndef consteval
-#define consteval constexpr
-#endif
-
+#  ifndef LIKELY
+#    define LIKELY
+#  endif
+#  ifndef UNLIKELY
+#    define UNLIKELY
+#  endif
+#  ifndef consteval
+#    define consteval constexpr
+#  endif
 #endif
 
 
@@ -96,6 +90,7 @@ namespace xlog
 
     typedef std::unordered_set<std::string> str_uset_t;
     typedef str_umap<std::string> arg_map_t;
+    typedef std::pair<std::string, std::string> str_pair_t;
     typedef std::streambuf* buffer_t;
     typedef std::filebuf* fbuffer_t;
     typedef unsigned int uint;
